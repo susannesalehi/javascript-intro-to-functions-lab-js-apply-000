de362a7c6ce777a66930fb3672540e05065171e8
Susanne Salehi <salehi.susanne@gmail.com>
2018-08-08 03:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ var lowercase = 'hello!'
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) console.log("I can't hear you!")
-} else if (string === string.toUpperCase()) console.log("YES INDEED!") 
+} if (string === string.toUpperCase()) console.log("YES INDEED!") 
 }
