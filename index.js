b526a23ca82be5daa2ea1801c11906504a3ca218
Susanne Salehi <salehi.susanne@gmail.com>
2018-08-08 03:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function logWhisper(string) {
 var lowercase = 'hello!'
 
 function sayHiToGrandma(string) {
-  if (lowercase.toLowerCase() === lowercase) return console.log("I can't hear you!")
+  if (string) return console.log("I can't hear you!")
 }
